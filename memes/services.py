a8aeba717712a171bd2d6619839498fa22c052da
tmanning <50249267+therealtmanning@users.noreply.github.com>
2019-05-18 15:33:10 -0400

    Update services.py
    random and trumpquotes

--- memes/services.py
+++ memes/services.py
@@ -12,3 +12,19 @@ def get_meme_options():
         options.append((result[key][(result[key].rfind('/')+1):], key))
 
     return options
+
+def randomMemeQuote(quote,meme):
+    #Pass an array of quotes and an array of memes
+    return,random.choice(quote),random.choice(meme)
+
+def getTrumpQuotes():
+    
+    trumpQuotes = []
+#Generate 20 random Trump Quotes
+    for i in range(20):
+        
+        url = 'https://api.tronalddump.io/random/quote'
+        r = requests.get(url).json()['value']
+        trumpQuotes.append(r)
+
+    return trumpQuotes
