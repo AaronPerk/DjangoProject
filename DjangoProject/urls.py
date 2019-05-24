4fff5a74b2b14d4e426e7e91bd172e8a1bf2bcea
Aaron Perkins <aperkins@glnt.local>
2019-05-24 10:20:58 -0400

    Fixed minor logout bug introduced in last commit

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,11 +1,12 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     url(r'^$', include('memes.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', LoginView.as_view(redirect_authenticated_user=True)),
+    url(r'^accounts/logout/$', LogoutView.as_view()),
     url(r'^memes/', include('memes.urls'))
 ]
